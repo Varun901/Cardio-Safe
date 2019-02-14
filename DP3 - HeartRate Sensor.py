2cec266818f6706ce9d22ba70732f6a3f0cbb9ff
Varun Jain <37890928+Varun901@users.noreply.github.com>
2019-02-14 15:43:45 -0500

    Updated Sensor Code

--- DP3 - HeartRate Sensor.py
+++ DP3 - HeartRate Sensor.py
@@ -1,37 +1,46 @@
-class HRThread(threading.Thread):
-    def __init__(self, queue):
-        self.q = queue
-        super().__init__()
-    def run(self):
-        hr = max30100()
-        while True:
+##Temperature Code: Mariam Dawood
+##Heartrate / SPO2 w Code: Mithil
+
+import PCF8591 as ADC
+import RPi.GPIO as GPIO
+import time
+import math
+from MPU6050 import MPU6050
+from max30100 import MAX30100
+from time import sleep
+from queue import Queue
+from datetime import datetime
+import threading
+
+
+# class temp _user(object):
+# def __init__(self,
+def run():
+    hr = MAX30100()
+    count = 0
+    while True:
+        hr.update()
+        #        print("LOOP", count)
+        bpm = hr.get_bpm()
+        average_bpm = hr.get_avg_bpm()
+        spo2 = hr.calculate_spo2()
+        if count % 200 == 0:
             out_file = open('HROutput.txt', "a+")
-            hr.update()
-            average_bpm = hr.get_avg_bpm()
-            value = hr.get_bpm()
-            out_file.write(str(value) + "\n")
-            self.q.put_nowait(value)
-            if value > average_bpm + 20 or value < average_bpm - 20:
-                print("Abnormal HeartRate Detected")
-                out_file.write("Abnormal HeartRate Detected ")
-                out_file.write(str(datetime.now()))
-                out_file.write("\n")
+            out_file_spo2 = open('SPO2Output.txt', "a+")
+            if bpm != None:
+                print(bpm)
+            print(average_bpm)
+            if average_bpm != None and bpm != None:
+                if bpm > (average_bpm + 20) or bpm < (average_bpm - 20):
+                    print("Abnormal HeartRate Detected " + "\n" + str(datetime.now()))
+            print(spo2)
+            if spo2 < 90:
+                print("Abnormal SPO2 Levels Detected " + "\n" + str(datetime.now()))
             out_file.close()
-            out_file = open('SPO2Output.txt', "a+")
-            hr.update()
-            value = hr.calculate_spo2()
-            out_file.write(str(value) + "\n")
-            self.q.put_nowait(value)
-            if value < 90:
-                print("Abnormal SPO2 Levels Detected")
-                out_file.write("Abnormal SPO2 levels Detected ")
-                out_file.write(str(datetime.now()))
-                out_file.write("\n")
-            out_file.close()
-            sleep(0.5)
-'''
-a = SoundThread()
-b = IMUThread()
-a.start()
-b.start()
-while True: pass
+            out_file_spo2.close()
+
+        count += 1
+        sleep(0.01)
+
+
+run()
